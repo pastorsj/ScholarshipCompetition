37660ef065f4eb0c78ecbdf96b137001b81eb065
pastorsj <sampastoriza@yahoo.com>
2022-05-04 14:19:45 -0400

    Initial work on website using creative tim

--- data-sourcing.py
+++ data-sourcing.py
@@ -1,10 +1,48 @@
 import pandas as pd
 import requests
+import re
 
-contracts_df = pd.read_csv('./CleanedContracts/cleaned_contracts.csv')
-print(contracts_df.head())
+# contracts_df = pd.read_csv('./CleanedContracts/cleaned_contracts.csv')
+# print(contracts_df.head())
+# print(contracts_df.columns)
 
-contracts_of_interest = ["CONT_AWD_HR001117C0025_9700_-NONE-_-NONE-"]
+# contracts_of_interest = ["CONT_AWD_HR001117C0025_9700_-NONE-_-NONE-"]
+
+# energy_keywords = ['solar', 'wind', 'renewable', 'thermal', 'hydroelectric',
+#                    'biomass', 'hydropower', 'ethanol', 'methanol']
+# energy_regex = re.compile('|'.join(energy_keywords), re.IGNORECASE)
+
+# secondary_energy_keywords = ['energy', 'storage']
+# secondary_energy_regex = re.compile(
+#     '|'.join(secondary_energy_keywords), re.IGNORECASE)
+
+# remove_keywords = ['windows']
+# remove_filter_regex = re.compile(
+#     '|'.join(remove_keywords), re.IGNORECASE)
+
+# energy_filtered_df = contracts_df[contracts_df['award_description'].apply(
+#     lambda x: bool(energy_regex.search(x)) if x and isinstance(x, str) else False)]
+
+# print('Results after first filter')
+# print(energy_filtered_df.head())
+# print(len(energy_filtered_df))
+
+# energy_filtered_df = energy_filtered_df[energy_filtered_df['award_description'].apply(
+#     lambda x: bool(secondary_energy_regex.search(x)) if x and isinstance(x, str) else False)]
+
+# print('Results after second filter')
+# print(energy_filtered_df.head())
+# print(len(energy_filtered_df))
+
+# energy_filtered_df = energy_filtered_df[energy_filtered_df['award_description'].apply(
+#     lambda x: not bool(remove_filter_regex.search(x)) if x and isinstance(x, str) else False)]
+
+# print('Results after final filter')
+# print(energy_filtered_df.head())
+# print(len(energy_filtered_df))
+
+# energy_filtered_df.to_csv(
+#     './CleanedContracts/energy_contracts.csv', index=False)
 
 
 def get_sub_awards(award_id):
@@ -38,7 +76,17 @@ def get_sub_awards(award_id):
         page += 1
 
     print("Retrieved ", len(output), " subawards for ", award_id)
-    return output
+    return pd.json_normalize(output)
 
 
-get_sub_awards(contracts_of_interest[0])
+energy_contracts_df = pd.read_csv('./CleanedContracts/energy_contracts.csv')
+
+sub_contracts_df = pd.DataFrame()
+for contract_id in set(energy_contracts_df['contract_award_unique_key'].tolist()):
+    sub_contracts = get_sub_awards(contract_id)
+    sub_contracts_df = pd.concat([sub_contracts_df, sub_contracts])
+
+print('All subawards', sub_contracts_df)
+print('Total number of sub contracts', len(sub_contracts_df))
+
+sub_contracts_df.to_csv('./CleanedContracts/energy_subcontracts.csv', index=False)
